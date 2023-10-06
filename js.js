0332cb5085f264c2a2908a631258475ede53deaf
Laraujo16 <145404647+Laraujo16@users.noreply.github.com>
2023-10-06 17:06:10 -0300

    Update js.js
    Agrego JS para trabajanosotros

---
+++ js.js
@@ -0,0 +1,16 @@
+//JS de Trabajanosotros//
+function borrar () {
+    document.getElementById("input1").value = ""}
+
+function borrar2 () {
+        document.getElementById("input2").value = ""}
+
+
+function borrar3 () {
+            document.getElementById("email1").value = ""}
+            
+function borrar4 () {
+                document.getElementById("com").value = ""}
+
+                
+    
